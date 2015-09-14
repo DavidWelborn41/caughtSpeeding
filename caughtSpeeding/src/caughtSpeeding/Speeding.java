aaa3e30174e74497f82a20fa7931bc0c5c2c60e5
David Welborn <david.welborn41@gmail.com>
2015-09-14 08:41:53 -0400

    did the stuff

--- caughtSpeeding/src/caughtSpeeding/Speeding.java
+++ caughtSpeeding/src/caughtSpeeding/Speeding.java
@@ -7,10 +7,17 @@ public class Speeding {
 		System.out.print(caughtSpeeding(60, false)); // should return 0
 		System.out.print(caughtSpeeding(65, false)); // should return 1
 		System.out.print(caughtSpeeding(65, true)); // should return 0
+		
+		
 	}
 	
 	public static int caughtSpeeding(int speed, boolean isBirthday)
 	{
-		return 0;
+		if(speed > 64 && isBirthday == false){
+			return 1;
+		}else{
+			return 0;
+		}
+	
 	}
 }
