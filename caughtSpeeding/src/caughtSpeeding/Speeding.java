7666af7c4f8dd78d317d6db455e2c91f89784f27
David Welborn <david.welborn41@gmail.com>
2015-10-24 13:03:53 -0400

    Did it

--- caughtSpeeding/src/caughtSpeeding/Speeding.java
+++ caughtSpeeding/src/caughtSpeeding/Speeding.java
@@ -11,13 +11,19 @@ public class Speeding {
 		
 	}
 	
-	public static int caughtSpeeding(int speed, boolean isBirthday)
-	{
-		if(speed > 64 && isBirthday == false){
-			return 1;
-		}else{
-			return 0;
+	public static int caughtSpeeding(int speed, boolean isBirthday) {
+		  if (!(isBirthday)) {
+		    if (speed <= 60)
+		      return 0;
+		    if (speed > 60 && speed <= 80)
+		      return 1;
+		    else
+		      return 2;
+		  } else if (speed <=65)
+		      return 0;
+		    else if (speed > 65 && speed <= 85)
+		      return 1;
+		    else
+		      return 2;
 		}
-	
-	}
 }
